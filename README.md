601f67fb22d7bdee3eba98fd8877f99f9fd9cbc1
pastorhudson@gmail.com <emerg07ams>
2018-08-21 15:19:55 -0400

    "AirPlay Stuff"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pythonista-airplay
 #### Demonstration of using two screens via airplay in pythonista
-Thanks to [Zrzka](https://github.com/zrzka) for the initial code posted to Pythonista forums.
+Thanks to [Zrzka](https://github.com/zrzka) for the initial code posted to Pythonista forums showing an HDMI example.
 I've added two simple buttons that increment a label on the second screen.
 
 #### To Test
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import ui
-import hdmi
+import airplay
 import objc_util
 
 
-def scoreUp(sender):
-	global score
-	score += 1
-	v['score'].text = str(score)
-	hdmi_view['score'].text = str(score)
-	
-def scoreDown(sender):
-	global score
-	score -= 1
-	v['score'].text = str(score)
-	hdmi_view['score'].text = str(score)
+def score_up(sender):
+    global score
+    score += 1
+    v['score'].text = str(score)
+    airplay_view['score'].text = str(score)
+
+
+def score_down(sender):
+    global score
+    score -= 1
+    v['score'].text = str(score)
+    airplay_view['score'].text = str(score)
+
 
 v = ui.load_view()
 
@@ -23,18 +25,16 @@ score = 0
 UIScreen = objc_util.ObjCClass('UIScreen')
 if len(UIScreen.screens()) > 1:
     second_screen = UIScreen.screens()[1]
-    #print(second_screen.mirroredScreen.obj)
-    
+    # print(second_screen.mirroredScreen.obj)
+
     bounds = second_screen.bounds()
-    
+
     UIWindow = objc_util.ObjCClass('UIWindow')
     second_window = UIWindow.alloc().initWithFrame_(bounds)
     second_window.setScreen(second_screen)
     second_window.setHidden(False)
-    
-    hdmi_view = hdmi.load_view()
-    hdmi_view_objc = objc_util.ObjCInstance(hdmi_view)
-    hdmi_view_objc.setFrame(second_window.bounds())
-    second_window.addSubview(hdmi_view_objc)
-    
 
+    airplay_view = airplay.load_view()
+    airplay_view_objc = objc_util.ObjCInstance(airplay_view)
+    airplay_view_objc.setFrame(second_window.bounds())
+    second_window.addSubview(airplay_view_objc)
